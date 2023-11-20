aba16b680f070ff323dfc18137a18345e37ddd8b
Christian Luca <Giovanni.luca@studio.unibo.it>
2023-11-20 18:29:38 +0100

    Finished part 1 & 2 of ex. no. 93.

--- 93-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 93-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,39 @@
 package it.unibo.mvc;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
 
 /**
  * Application controller. Performs the I/O.
  */
 public class Controller {
 
+    private String pathFile;
+
+    public Controller() {
+        this.pathFile = System.getProperty("user.home") + System.getProperty(File.separator) + "output.txt";
+    }
+
+    public Controller(String filePath) {
+        this.pathFile = filePath;
+    }
+
+    private File getMyFile() {
+        return new File(this.pathFile);
+    }
+
+    private String pathCurrentFile() {
+        return this.pathFile;
+    }
+
+    void writeStringOnCurrentFile(String content) throws IOException{
+        try {
+            PrintStream ps = new PrintStream(pathFile, StandardCharsets.UTF_8);
+            ps.println(content);
+        } catch (IOException e) {
+            System.out.println("Error.");
+        }
+    }
 }
--- 93-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 93-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -1,6 +1,14 @@
 package it.unibo.mvc;
 
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 /**
  * A very simple program using a graphical interface.
@@ -8,6 +16,39 @@ import javax.swing.JFrame;
  */
 public final class SimpleGUI {
 
-    private final JFrame frame = new JFrame();
 
+    private final JFrame frame = new JFrame("My first java graphical interface");
+    private Controller myController = new Controller();
+
+    public SimpleGUI() {
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JPanel myPanel = new JPanel(new BorderLayout());
+        frame.getContentPane().add(myPanel);
+        JTextArea myTextArea = new JTextArea();
+        myPanel.add(myTextArea, BorderLayout.NORTH);
+        JButton myButton = new JButton("Save");
+        myButton.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    myController.writeStringOnCurrentFile(myTextArea.getText());
+                } catch (IOException e1) {
+                    System.out.println("Error");
+                    e1.printStackTrace();
+                }
+            }
+            
+        });
+        myPanel.add(myButton, BorderLayout.SOUTH);
+    }
+
+    private void display() {
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new SimpleGUI().display();
+    }
 }
