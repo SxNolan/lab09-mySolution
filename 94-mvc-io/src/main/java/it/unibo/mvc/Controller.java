3818e09da6b7811c3e7aff0fffc4027e9a7b2c0d
Christian Luca <Giovanni.luca@studio.unibo.it>
2023-11-21 13:52:23 +0100

    Finished Controller.java interface.

--- 94-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 94-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,8 +1,18 @@
 package it.unibo.mvc;
 
+import java.util.List;
+
 /**
  *
  */
 public interface Controller {
+
+    void setNextString();
+
+    void getNextString();
+
+    List<String> getHistoryPrintedStrings();
+
+    void printCurrentString();
 
 }
