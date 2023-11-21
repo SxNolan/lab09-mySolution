a8535627382514c6b184401b087b03e1987a55af
Christian Luca <Giovanni.luca@studio.unibo.it>
2023-11-21 14:28:55 +0100

    Completed ex. no. 94.

--- 94-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 94-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -1,6 +1,14 @@
 package it.unibo.mvc;
 
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 /**
  * A very simple program using a graphical interface.
@@ -9,5 +17,52 @@ import javax.swing.JFrame;
 public final class SimpleGUI {
 
     private final JFrame frame = new JFrame();
+    Controller myController = new SimpleController();
+
+    public SimpleGUI() {
+        JPanel myPanel = new JPanel(new BorderLayout());
+        frame.getContentPane().add(myPanel);
+        JTextField myTextField = new JTextField();
+        myPanel.add(myTextField, BorderLayout.NORTH);
+        JTextArea myTextArea = new JTextArea();
+        myPanel.add(myTextArea, BorderLayout.CENTER);
+        JPanel buttonPanel = new JPanel(new FlowLayout());
+        JButton printButton = new JButton("Print");
+        printButton.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                myController.setNextString(myTextField.getText());
+                myController.printCurrentString();
+            }
+            
+        });
+        
+        buttonPanel.add(printButton);
+        JButton showHistoryButton = new JButton("Show history");
+        showHistoryButton.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                for (String elem : myController.getHistoryPrintedStrings()) {
+                    myTextArea.append(elem + " | ");
+                }
+            }
+            
+        });
+        
+        buttonPanel.add(showHistoryButton);
+        myPanel.add(buttonPanel, BorderLayout.SOUTH);
+    }
+
+    void display() {
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new SimpleGUI().display();
+    }
 
 }
